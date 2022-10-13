2d6f502a2b717847a8d6d2e8e88d941950f0ef51
southpaw203 <73884858+vlaex@users.noreply.github.com>
2022-10-13 15:18:36 +0300

    add the rankings endpoint, update graphql

--- app/routes/brainly_users.py
+++ app/routes/brainly_users.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
 
-from app.models import GetBrainlyUsersPayload, SendMessageToUserPayload
-from app.brainly_api import legacy_api
+from app.models import GetBrainlyUsersPayload, GetBrainlyUsersWithExtraDataPayload, \
+    SendMessageToUserPayload
+from app.brainly_api import legacy_api, graphql_api
 
 
 router = APIRouter(prefix='/brainly/users')
@@ -23,6 +24,47 @@ async def get_users(payload: GetBrainlyUsersPayload):
         })
 
     return users
+
+USER_WITH_EXTRA_DATA_FRAGMENT = """
+fragment UserWithExtraData on User {
+  nick
+  avatar {url}
+  gender
+  points
+  statistics {banCount warningCount}
+  thanks {count}
+  answerCountBySubject {
+    count
+    markedAsBest
+    subject {id name}
+  }
+  answers {
+    count
+  }
+  created
+  rank {name id}
+  specialRanks {name id}
+  grade {id name}
+}
+"""
+
+@router.post('/extra_data')
+async def get_users_with_extra_data(payload: GetBrainlyUsersWithExtraDataPayload):
+    fetched_users = await graphql_api.mapped_query_with_ids(
+        payload.ids,
+        'user',
+        '...UserWithExtraData',
+        transform_entry=lambda user: user,#{
+        #    'nick': user['nick'],
+        #    'avatar': '' if user['avatar'] is None else user['avatar']['url'],
+        #    'thanks_count': user['thanks']['count'],
+        #    'gender': user['gender'],
+        #    'points': user['points'],
+        #},
+        extra_query=USER_WITH_EXTRA_DATA_FRAGMENT
+    )
+
+    return fetched_users
 
 
 @router.post('/send_message')
